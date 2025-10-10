e98cd5792b4eb598894ca7820fedb9279a7ead8f
petergeelhoed <petergeelhoed@gmail.com>
2025-10-10 08:56:23 +0200

    add move tests

--- test.cpp
+++ test.cpp
@@ -29,6 +29,24 @@ int main()
             perror("writefailure");
             exit(-1);
         }
+
+        auto fd3 = std::move(fd);
+        if (!fd3.write(byteVector))
+        {
+            perror("writefailure3");
+            exit(-1);
+        }
+
+        // use moved from fd, it is illegal and flagged by clang-tidy
+        auto fd4(std::move(fd));
+        if (!fd4.write(byteVector))
+        {
+            // Will error out with Bad file descriptor
+            // as it is set to -1  by the move constructor
+            perror("This is expected: writefailure4");
+            // No exit because this is expected
+            // exit(-1);
+        }
     }
 
     // this is impossible You need to use the open
