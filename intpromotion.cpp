d7c881d23a3836f36ceac4cef10284437c0243e7
petergeelhoed <petergeelhoed@gmail.com>
2025-11-05 14:12:58 +0100

    7/5

--- intpromotion.cpp
+++ intpromotion.cpp
@@ -2,37 +2,37 @@
 #include <climits>
 #include <cstdio>
 
-extern const int SIZE = 1350;
+extern const int SIZE = 5;
 extern const int CENT = 100;
+long longSIZE = SIZE;
+long longCENT = 100;
 int verbose;
+
+const char red[9] = "\033[31m";
+const char green[9] = "\033[32m";
+const char equal[3] = "==";
+const char inequal[3] = "!=";
 
 int f(int value)
 {
     auto result = (int)((float)value / (float)SIZE * 100.0);
 
     float tmp3 = (float)value;
-    if (verbose)
-        printf("float   %.12f\n", tmp3);
-
     float tmp2 = (float)SIZE;
-    if (verbose)
-        printf("float   %.12f\n", tmp2);
 
     // division of two floats gives a float
-    // in the example of 27/1350 this division will be
-    // 0.01999999955296516418
+    // in the example of 7/5 this division will be
+    // off at the 8'th digit.
     float tmp1 = tmp3 / tmp2;
     if (verbose)
-        printf("float / %.12f\n", tmp1);
+        printf("float %.6f / %.6f  =  %.8f\n\n", tmp3, tmp2, tmp1);
 
     // 100.0 is a double literal
     double tmp0 = tmp1 * 100.0;
-    if (verbose)
-        printf("double  %.18lf\n", tmp0);
 
-    // and then this cast will be 1 instead of 2.
+    // 1.39999998 will be cast to 139
     int res = (int)tmp0;
-    return result;
+    return res;
 }
 
 int g(int value)
@@ -55,26 +55,25 @@ int main(int argc, char** argv)
     verbose = (argc != 1);
     int i = 30;
 
-    while (i > 0)
+    while (i > -30)
     {
         long longi = i;
-        long longSIZE = SIZE;
-        long longCENT = 100;
-        const char red[9] = "\033[31m";
-        const char green[9] = "\033[32m";
-        const char equal[3] = "==";
-        const char inequal[3] = "!=";
+        long li = longCENT * longi / longSIZE;
+
+        int fi = f(i);
+        int gi = g(i);
+        bool good = (fi == gi);
 
         printf("val=%11d SIZE=%11d %sf(val)=%11d %s "
                "g(val)=%11d\033[0m ==> "
                "(calc as long)%ld\n",
                i,
                SIZE,
-               (f(i) != g(i)) ? red : green,
-               f(i),
-               (f(i) != g(i)) ? inequal : equal,
-               g(i),
-               longCENT * longi / longSIZE);
+               good ? green : red,
+               fi,
+               good ? equal : inequal,
+               gi,
+               li);
         i -= 1;
     }
     return 0;
