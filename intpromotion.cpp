17df91c690911bfdff0f1421c93c01e72a3c6a9e
petergeelhoed <petergeelhoed@gmail.com>
2025-11-05 14:41:31 +0100

    explanation

--- intpromotion.cpp
+++ intpromotion.cpp
@@ -4,9 +4,9 @@
 
 extern const int SIZE = 5;
 extern const int CENT = 100;
-long longSIZE = SIZE;
-long longCENT = 100;
-int verbose;
+const long longSIZE = SIZE;
+const long longCENT = 100;
+int verbose = 0;
 
 const char red[9] = "\033[31m";
 const char green[9] = "\033[32m";
@@ -24,17 +24,35 @@ int f(int value)
     // in the example of 7/5 this division will be
     // off at the 8'th digit.
     float tmp1 = tmp3 / tmp2;
-    if (verbose)
-        printf("float %.6f / %.6f  =  %.8f\n\n", tmp3, tmp2, tmp1);
 
     // 100.0 is a double literal
     double tmp0 = tmp1 * 100.0;
 
-    // 1.39999998 will be cast to 139
-    int res = (int)tmp0;
+    // for 7/5 = 1.39999998 will be cast to 139
+    int res = (int)(tmp0);
+
+    // better would be to add 0.5 for positive tmp0
+    // and subtract for negative
+    // that would then still fail for 671088/5 as the representation error would
+    // be 13421759.375000 and not the 1342176 expected.
+    // And that will no be fixed by +0.5
+
+    double corr = (tmp1 >= 0) ? 0.5 : -0.5;
+    int rescorr = (int)(tmp0 + corr);
+    if (verbose)
+        printf("float %.6f / %.6f  =  %.8f\ndouble:%.15f uncorrected: %d "
+               "corrected:%d\n",
+               tmp3,
+               tmp2,
+               tmp1,
+               tmp0,
+               res,
+               rescorr);
+
     return res;
 }
 
+// So this is how you should do it:
 int g(int value)
 {
     // f() seems to calculate a percentage
@@ -52,10 +70,10 @@ int g(int value)
 
 int main(int argc, char** argv)
 {
-    verbose = (argc != 1);
-    int i = 30;
+    verbose = (argc > 1);
+    int i = 1;
 
-    while (i > -30)
+    while (i < INT_MAX / CENT)
     {
         long longi = i;
         long li = longCENT * longi / longSIZE;
@@ -74,7 +92,7 @@ int main(int argc, char** argv)
                good ? equal : inequal,
                gi,
                li);
-        i -= 1;
+        i = (i < 32) ? i + 1 : i * 2;
     }
     return 0;
 }
