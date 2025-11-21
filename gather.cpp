fde0c10495f1dbd8f615ddb1cd931c4f8b5342d5
petergeelhoed <petergeelhoed@gmail.com>
2025-11-21 11:34:52 +0100

    works in two statements

--- gather.cpp
+++ gather.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <iostream>
 #include <vector>
 
@@ -10,6 +11,12 @@ void gather(std::vector<Point>::iterator begin,
             std::vector<Point>::iterator end,
             std::vector<Point>::iterator pivot)
 {
+    std::stable_partition(begin, pivot, [&pivot](const Point& point) {
+        return pivot->x != point.x;
+    });
+    std::stable_partition(pivot, end, [&pivot](const Point& point) {
+        return pivot->x == point.x;
+    });
 }
 
 void printVec(const std::vector<Point>& points)
@@ -25,6 +32,8 @@ int main()
     std::vector<Point> points = {
         {1, 2}, {4, 3}, {3, 9}, {4, 2}, {1, 4}, {4, 8}, {4, 6}};
 
+    printVec(points);
+    std::cout << "\n";
     gather(points.begin(), points.end(), points.begin() + 3);
     printVec(points);
 }
