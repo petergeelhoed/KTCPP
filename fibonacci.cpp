6051efc81089e80b0679341a64bdb6e14c2f15d7
petergeelhoed <petergeelhoed@gmail.com>
2025-12-03 07:19:01 +0100

    exercise two

--- fibonacci.cpp
+++ fibonacci.cpp
@@ -3,6 +3,27 @@
 
 namespace
 {
+unsigned int fib_lin(unsigned int n)
+{
+    if (n < 2)
+    {
+        return n;
+    }
+
+    unsigned int f1 = 1;
+    unsigned int f2 = 1;
+    for (auto f : std::views::iota(2U, n) |
+                      std::views::transform(
+                          [&f1, &f2]([[maybe_unused]] const unsigned int i) {
+                              return f1 + f2;
+                          }))
+    {
+        f1 = f2;
+        f2 = f;
+    }
+    return f2;
+}
+
 unsigned int fib(const unsigned int n) // NOLINT[misc-no-recursion]()
 {
     if (n == 0)
@@ -20,6 +41,15 @@ int main()
     auto r = std::views::iota(beginrange, endrange + 1) |
              std::views::transform([](const unsigned int i) { return fib(i); });
     for (auto f : r)
+    {
+        std::cout << f << '\n';
+    }
+    std::cout << '\n';
+
+    auto q =
+        std::views::iota(beginrange, endrange + 1) |
+        std::views::transform([](const unsigned int i) { return fib_lin(i); });
+    for (auto f : q)
     {
         std::cout << f << '\n';
     }
