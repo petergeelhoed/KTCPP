a7d568eca9fe191ce9e3a43e9ac5539b17b0a1e6
petergeelhoed <petergeelhoed@gmail.com>
2025-12-03 11:37:02 +0100

    ex3

--- fibonacci.cpp
+++ fibonacci.cpp
@@ -1,9 +1,32 @@
 #include <iostream>
+#include <numeric>
 #include <ranges>
+#include <utility>
 
 namespace
 {
-unsigned int fib_lin(unsigned int n)
+unsigned int fib_accumulate(const unsigned int n)
+{
+    if (n < 2)
+    {
+        return n;
+    }
+
+    auto range = std::views::iota(2U, n + 1);
+
+    auto result = std::accumulate(
+        range.begin(),
+        range.end(),
+        std::pair{0U, 1U},
+        // the val needs to be there for this to work
+        [](auto state, [[maybe_unused]] const unsigned int val) {
+            return std::pair{state.second, state.first + state.second};
+        });
+
+    return result.second;
+}
+
+unsigned int fib_lin(const unsigned int n)
 {
     if (n < 2)
     {
@@ -50,6 +73,16 @@ int main()
         std::views::iota(beginrange, endrange + 1) |
         std::views::transform([](const unsigned int i) { return fib_lin(i); });
     for (auto f : q)
+    {
+        std::cout << f << '\n';
+    }
+
+    std::cout << '\n';
+
+    auto s = std::views::iota(beginrange, endrange + 1) |
+             std::views::transform(
+                 [](const unsigned int i) { return fib_accumulate(i); });
+    for (auto f : s)
     {
         std::cout << f << '\n';
     }
